4850f2821104151cf12d7a1178fc66ddf84b0c75
Jose Osuna <2281302+joseosuna-engineer@users.noreply.github.com>
2021-01-13 19:00:36 -0300

    adds initial app structure.

---
+++ src/main/java/com/prottonne/deserialization/DeserializationApplication.java
@@ -0,0 +1,13 @@
+package com.prottonne.deserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeserializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeserializationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prottonne/deserialization/DeserializationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prottonne.deserialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeserializationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
